b61b55f60c7294f33a6fb64fb947b0eddee67187
Shicheng Chen <c337134154@gmail.com>
2021-03-07 21:38:51 +0800

    add RHD STB joints reading code

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='globalCamera',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/ShichengChen',
+      author='csc',
+      author_email='csc@example.com',
+      license='MIT',
+      packages=['globalCamera'],
+      zip_safe=False) 
